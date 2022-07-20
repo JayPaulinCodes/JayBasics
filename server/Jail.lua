3a73e8f389854364981e20e62716b269fb173ce0
JayPaulinCodes <jacobwpaulin@gmail.com>
2022-07-20 16:08:52 -0400

    Fixed File

--- server/Jail.lua
+++ server/Jail.lua
@@ -1,30 +1,5 @@
 if CONFIG["Jail"]["Enabled"] then
 
-<<<<<<< HEAD
-        TriggerClientEvent("Jay:Basics:openJailGUI", -1, _source)
-    end, false)
-
-end)
-
-RegisterServerEvent("Jay:Basics:jailPlayerRequest")
-AddEventHandler("Jay:Basics:jailPlayerRequest", function(NUIData, jail) 
-    
-    TriggerClientEvent('Jay:Basics:jailPlayer"', NUIData.id, jail, NUIData.length)
-
-    TriggerClientEvent('chat:addMessage', -1, 
-        { 
-            templateId = 'Jay:Basics:judgeSentence',
-            multiline = true, 
-            args = { 
-                NUIData.targetName,
-                math.floor((NUIData.length / 10)),
-                jail.name,
-                NUIData.reason
-            } 
-        } 
-    )
-end)
-=======
     Citizen.CreateThread(function ()
    
         Citizen.Wait(75)
@@ -36,5 +11,24 @@ end)
         end, false)
     
     end)
+
+    RegisterServerEvent("Jay:Basics:jailPlayerRequest")
+    AddEventHandler("Jay:Basics:jailPlayerRequest", function(NUIData, jail) 
+        
+        TriggerClientEvent('Jay:Basics:jailPlayer"', NUIData.id, jail, NUIData.length)
+
+        TriggerClientEvent('chat:addMessage', -1, 
+            { 
+                templateId = 'Jay:Basics:judgeSentence',
+                multiline = true, 
+                args = { 
+                    NUIData.targetName,
+                    math.floor((NUIData.length / 10)),
+                    jail.name,
+                    NUIData.reason
+                } 
+            } 
+        )
+    end)
+    
 end
->>>>>>> b260c64c2261cd1a0861fafe54015478eb936b8a
