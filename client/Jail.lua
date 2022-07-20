cd348f1350d04c21ecd0577c151d6e1e2e949a13
JayPaulinCodes <jacobwpaulin@gmail.com>
2022-07-20 16:31:20 -0400

    Refactored File

--- client/Jail.lua
+++ client/Jail.lua
@@ -37,67 +37,61 @@ if CONFIG["Jail"]["Enabled"] then
         SetNuiFocus(true, true)
     end)
 
--- Jail Request
-RegisterNUICallback("submitJailRequest", function(data)
-    TriggerServerEvent("Jay:Basics:jailPlayerRequest", data, getClosestStation())
-end)
+    -- Jail Request from HTML
+    RegisterNUICallback("submitJailRequest", function(data)
+        TriggerServerEvent("Jay:Basics:jailPlayerRequest", data, getClosestStation())
+    end)
 
--- Jail Player
-RegisterNetEvent("Jay:Basics:jailPlayer")
-AddEventHandler("Jay:Basics:jailPlayer", function(jail, length) 
-    
-    IsPlayerJailed = true
+    -- Jail Player
+    RegisterNetEvent("Jay:Basics:jailPlayer")
+    AddEventHandler("Jay:Basics:jailPlayer", function(jail, length) 
+        
+        IsPlayerJailed = true
 
-    switchPlayerToCoords(jail.cellCoords)
+        switchPlayerToCoords(jail.cellCoords)
 
-    Citizen.SetTimeout(length * 1000, function()
+        Citizen.SetTimeout(length * 1000, function()
 
-        IsPlayerJailed = false
-    
-        switchPlayerToCoords(jail.freedCoords)
-    
-        TriggerEvent('chat:addMessage', 
-            { 
-                templateId = 'Jay:Basics:wardenRelease',
-                multiline = true, 
-                args = {}
-            } 
-        )
+            IsPlayerJailed = false
+        
+            switchPlayerToCoords(jail.freedCoords)
+        
+            TriggerEvent('chat:addMessage', 
+                { 
+                    templateId = 'Jay:Basics:wardenRelease',
+                    multiline = true, 
+                    args = {}
+                } 
+            )
+
+        end)
 
     end)
 
-end)
+    -- LOOPS
+    Citizen.CreateThread(function()
+        while true do
+            Citizen.Wait(0)
 
+            if IsPlayerJailed then
 
-    -- Jail Request from HTML
-    RegisterNUICallback("submitJailRequest", function(data)
-        local targetName = data.targetName
-        local targetId = data.targetId
-        local reason = data.reason
-        local length = data.length
+                DisableControlAction(0, CONTROLS["INPUT_ATTACK"]["ControlIndex"], true)
+                DisableControlAction(0, CONTROLS["INPUT_ATTACK2"]["ControlIndex"], true) -- 257
+                DisableControlAction(0, CONTROLS["INPUT_AIM"]["ControlIndex"], true) -- 25
+                DisableControlAction(0, CONTROLS["INPUT_MELEE_ATTACK1"]["ControlIndex"], true) -- 263
+                DisableControlAction(0, CONTROLS["INPUT_RELOAD"]["ControlIndex"], true) -- 45
+                DisableControlAction(0, CONTROLS["INPUT_DETONATE"]["ControlIndex"], true)  -- 47
+                DisableControlAction(0, CONTROLS["INPUT_MELEE_ATTACK2"]["ControlIndex"], true) -- 264
+                DisableControlAction(0, CONTROLS["INPUT_MELEE_ATTACK_LIGHT"]["ControlIndex"], true) -- 140
+                DisableControlAction(0, CONTROLS["INPUT_MELEE_ATTACK_HEAVY"]["ControlIndex"], true) -- 141
+                DisableControlAction(0, CONTROLS["INPUT_MELEE_ATTACK_ALTERNATE"]["ControlIndex"], true) -- 142
+                DisableControlAction(0, CONTROLS["INPUT_MELEE_BLOCK"]["ControlIndex"], true) -- 143
+                DisableControlAction(0, CONTROLS["INPUT_SELECT_WEAPON"]["ControlIndex"], true) -- 37
 
--- LOOPS
-Citizen.CreateThread(function()
-	while true do
-		Citizen.Wait(0)
+            else
+                Citizen.Wait(500)
+            end
+        end
+    end)
 
-		if IsPlayerJailed then
-
-            DisableControlAction(0, CONTROLS["INPUT_ATTACK"]["ControlIndex"], true)
-            DisableControlAction(0, CONTROLS["INPUT_ATTACK2"]["ControlIndex"], true) -- 257
-            DisableControlAction(0, CONTROLS["INPUT_AIM"]["ControlIndex"], true) -- 25
-            DisableControlAction(0, CONTROLS["INPUT_MELEE_ATTACK1"]["ControlIndex"], true) -- 263
-            DisableControlAction(0, CONTROLS["INPUT_RELOAD"]["ControlIndex"], true) -- 45
-            DisableControlAction(0, CONTROLS["INPUT_DETONATE"]["ControlIndex"], true)  -- 47
-            DisableControlAction(0, CONTROLS["INPUT_MELEE_ATTACK2"]["ControlIndex"], true) -- 264
-            DisableControlAction(0, CONTROLS["INPUT_MELEE_ATTACK_LIGHT"]["ControlIndex"], true) -- 140
-            DisableControlAction(0, CONTROLS["INPUT_MELEE_ATTACK_HEAVY"]["ControlIndex"], true) -- 141
-            DisableControlAction(0, CONTROLS["INPUT_MELEE_ATTACK_ALTERNATE"]["ControlIndex"], true) -- 142
-            DisableControlAction(0, CONTROLS["INPUT_MELEE_BLOCK"]["ControlIndex"], true) -- 143
-            DisableControlAction(0, CONTROLS["INPUT_SELECT_WEAPON"]["ControlIndex"], true) -- 37
-
-		else
-			Citizen.Wait(500)
-		end
-	end
-end)
+end
